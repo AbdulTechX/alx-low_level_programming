16c03f87562805454d18b9047ac84ced4f050ce8
AbdultechX <abdulazeezbuhari27@gmail.com>
2023-03-20 05:06:03 -0700

    variable_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char ch = 97;
+
+	for (ch = 97; ch <= 122; ch++)
+		putchar(ch);
+	return (0);
+}
