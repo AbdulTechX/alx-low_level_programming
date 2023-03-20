fa237326b799d2da7d8cc314fd77227f3fa6efc1
AbdultechX <abdulazeezbuhari27@gmail.com>
2023-03-20 06:29:34 -0700

    fix code

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,10 @@ int main(void)
 	char i = 97;
 
 	for (i = 97; i <= 122; i++)
-		if (i == 101 || i == 113)
-		{
-			continue;
+	{
+		if (i != 101 && i != 113)
 			putchar(i);
-		}
+	}
 	putchar('\n');
 
 	return (0);
