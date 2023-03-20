9dc2193948216c29e4ff8baecafca4b5c17520f4
AbdultechX <abdulazeezbuhari27@gmail.com>
2023-03-20 07:02:47 -0700

    variable_if else

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		printf("%d", a);
+
+	printf("\n");
+
+	return (0);
+}
