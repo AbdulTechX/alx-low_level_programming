5ee19391d7b0607fdc8763841ac1e5bee685230c
AbdultechX <abdulazeezbuhari27@gmail.com>
2023-03-30 17:00:31 -0700

    fix code

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,10 +8,12 @@
  */
 char *cap_string(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] >= 'a' && str[i] <= 'z'; i++)
+	while (str[i])
 	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
 		if (str[i - 1] == ' ' ||
 		    str[i - 1] == '\t' ||
 		    str[i - 1] == '\n' ||
