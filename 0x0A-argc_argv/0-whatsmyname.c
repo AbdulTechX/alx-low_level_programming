bff5df074e9ac7680e0a85a10fa55d09944a2ad1
AbdulTechX <abdulazeezbuhari27@gmail.com>
2023-04-08 10:24:38 -0700

    arguement in c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints its name, followed by a new line.
+ * @argc: The number of arguments supplied to the program
+ * @agrv: an array of pointer to the arguements.
+ * Return: Always 0
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
